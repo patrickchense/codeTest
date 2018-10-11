e32b5540a45ede306aa75ee2890bb1afac929dad
chenzhe <zhe@eatigo.com>
2018-10-11 18:42:38 +0800

    pairsum

--- src/GoldmanSachs/PairSum.java
+++ src/GoldmanSachs/PairSum.java
@@ -13,11 +13,44 @@ first think:
     1. double for, brute force
     2. two pointers
 
-    TODO
+don't understand the questions,
+so I pick some leetcode questions similar to do
+
+https://leetcode.com/problems/map-sum-pairs/
+
+Implement a MapSum class with insert, and sum methods.
+
+For the method insert, you'll be given a pair of (string, integer). The string represents the key and the integer represents the value.
+If the key already existed, then the original key-value pair will be overridden to the new one.
+
+For the method sum, you'll be given a string representing the prefix, and you need to return the sum of all the pairs' value whose key starts with the prefix.
+
+Example 1:
+Input: insert("apple", 3), Output: Null
+Input: sum("ap"), Output: 3
+Input: insert("app", 2), Output: Null
+Input: sum("ap"), Output: 5
+
+
+
+https://leetcode.com/problems/find-k-pairs-with-smallest-sums/description/
+
  */
 public class PairSum {
 
     public static void main(String args[]) {
 
+    }
+
+    class MapSum {
+
+        String[] keys;
+        int[] vals;
+
+        void insert(String key, Integer val) {
+
+        }
+
+        int sum(String prefix) {}
     }
 }
