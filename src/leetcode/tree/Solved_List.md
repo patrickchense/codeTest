9745dc1770f47e1810cd0ec1724d2ecc8fb04cd5
chenzhe <zhe@eatigo.com>
2018-10-13 23:46:36 +0800

    add leetcode 30 add solved binary tree preorder & level order traversal

--- src/leetcode/tree/Solved_List.md
+++ src/leetcode/tree/Solved_List.md
@@ -8,4 +8,9 @@ https://leetcode.com/problems/n-ary-tree-preorder-traversal/description/
 n-ary tree level order
 https://leetcode.com/problems/n-ary-tree-level-order-traversal/description/
 
- 
+binary tree preorder traversal  144
+https://leetcode.com/problems/binary-tree-preorder-traversal/description/ 
+
+binary tree level order traversal 102
+https://leetcode.com/problems/binary-tree-level-order-traversal/description/ 
+
---
+++ src/leetcode/tree/SubstringConcatenatonWords.java
@@ -0,0 +1,68 @@
+package leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+leetcode 30
+https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+You are given a string, s, and a list of words, words, that are all of the same length. Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
+
+Example 1:
+
+Input:
+  s = "barfoothefoobarman",
+  words = ["foo","bar"]
+Output: [0,9]
+Explanation: Substrings starting at index 0 and 9 are "barfoor" and "foobar" respectively.
+The output order does not matter, returning [9,0] is fine too.
+Example 2:
+
+Input:
+  s = "wordgoodstudentgoodword",
+  words = ["word","student"]
+Output: []
+
+
+ */
+public class SubstringConcatenatonWords {
+
+
+    /*brute force always working
+        use Map to keep the words, add delete when matches
+
+        O(n * k), n = S.length, k = words.length,  O(n^k) space
+
+        optimize with map clear/putAll, use O(k) space
+
+    */
+    public List<Integer> findSubstring(String S, String[] words) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (S == null || words == null || words.length == 0) return res;
+        int len = words[0].length(); // length of each word
+
+        Map<String, Integer> map = new HashMap<>(); // map for L
+        for (String w : words) map.put(w, map.containsKey(w) ? map.get(w) + 1 : 1);
+        Map<String, Integer> copy = new HashMap<>(map);
+        for (int i = 0; i <= S.length() - len * words.length; i++) {
+            for (int j = 0; j < words.length; j++) { // checkc if match
+                String str = S.substring(i + j * len, i + j * len + len); // next word
+                if (copy.containsKey(str)) { // is in remaining words
+                    int count = copy.get(str);
+                    if (count == 1) copy.remove(str);
+                    else copy.put(str, count - 1);
+                    if (copy.isEmpty()) { // matches
+                        res.add(i);
+                        copy.putAll(map);
+                        break;
+                    }
+                } else break; // not in L
+            }
+            copy.clear();
+            copy.putAll(map);
+        }
+        return res;
+    }
+}
