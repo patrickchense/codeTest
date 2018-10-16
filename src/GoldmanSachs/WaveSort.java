27b200a68c4fcbfd3452ab2ac52d27e86b0f3778
chenzhe <zhe@eatigo.com>
2018-10-16 12:05:50 +0800

    add Goldman sachs(wave sort)

---
+++ src/GoldmanSachs/WaveSort.java
@@ -0,0 +1,92 @@
+package GoldmanSachs;
+
+import java.util.Arrays;
+
+/*
+https://www.geeksforgeeks.org/sort-array-wave-form-2/
+
+Given an unsorted array of integers, sort the array into a wave like array. An array ‘arr[0..n-1]’ is sorted in wave form if arr[0] >= arr[1] <= arr[2] >= arr[3] <= arr[4] >= …..
+Examples:
+
+ Input:  arr[] = {10, 5, 6, 3, 2, 20, 100, 80}
+ Output: arr[] = {10, 5, 6, 2, 20, 3, 100, 80} OR
+                 {20, 5, 10, 2, 80, 6, 100, 3} OR
+                 any other array that is in wave form
+
+ Input:  arr[] = {20, 10, 8, 6, 4, 2}
+ Output: arr[] = {20, 8, 10, 4, 6, 2} OR
+                 {10, 8, 20, 2, 6, 4} OR
+                 any other array that is in wave form
+
+ Input:  arr[] = {2, 4, 6, 8, 10, 20}
+ Output: arr[] = {4, 2, 8, 6, 20, 10} OR
+                 any other array that is in wave form
+
+ Input:  arr[] = {3, 6, 5, 10, 7, 20}
+ Output: arr[] = {6, 3, 10, 5, 20, 7} OR
+                 any other array that is in wave form
+
+solution:
+Simple Solution is to use sorting. First sort the input array, then swap all adjacent elements.
+O(nlogn)
+For example, let the input array be {3, 6, 5, 10, 7, 20}. After sorting, we get {3, 5, 6, 7, 10, 20}. After swapping adjacent elements, we get {5, 3, 7, 6, 20, 10}.
+
+@sort
+
+solution2: O(n)
+raverse all even positioned elements of input array, and do following.
+….a) If current element is smaller than previous odd element, swap previous and current.
+….b) If current element is smaller than next odd element, swap next and current.
+
+
+ */
+public class WaveSort {
+
+    void swap(int arr[], int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    // This function sorts arr[0..n-1] in wave form, i.e.,
+    // arr[0] >= arr[1] <= arr[2] >= arr[3] <= arr[4]..
+    // O(nlogn)
+    void sortInWave(int arr[], int n) {
+        // Sort the input array
+        Arrays.sort(arr);
+
+        // Swap adjacent elements
+        for (int i = 0; i < n - 1; i += 2)
+            swap(arr, i, i + 1);
+    }
+
+    // This function sorts arr[0..n-1] in wave form, i.e.,
+    // arr[0] >= arr[1] <= arr[2] >= arr[3] <= arr[4]....
+    // O(n)
+    void sortInWave2(int arr[], int n)
+    {
+        // Traverse all even elements
+        for (int i = 0; i < n; i+=2)
+        {
+            // If current even element is smaller
+            // than previous
+            if (i>0 && arr[i-1] > arr[i] )
+                swap(arr, i-1, i);
+
+            // If current even element is smaller
+            // than next
+            if (i<n-1 && arr[i] < arr[i+1] )
+                swap(arr, i, i + 1);
+        }
+    }
+
+    // Driver method
+    public static void main(String args[]) {
+        WaveSort ob = new WaveSort();
+        int arr[] = {10, 90, 49, 2, 1, 5, 23};
+        int n = arr.length;
+        ob.sortInWave(arr, n);
+        for (int i : arr)
+            System.out.print(i + " ");
+    }
+}
