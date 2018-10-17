1d4e7867f709167170d2c47a32731bac0ce6699c
chenzhe <zhe@eatigo.com>
2018-10-18 07:25:25 +0800

    add daily 1018

---
+++ src/dailyproblem/Daily20181018.java
@@ -0,0 +1,12 @@
+package dailyproblem;
+
+/*
+The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a Monte Carlo method.
+
+Hint: The basic equation of a circle is x2 + y2 = r2.
+
+@Google
+
+ */
+public class Daily20181018 {
+}
