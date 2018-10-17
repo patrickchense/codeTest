43af9342c2a3ff9292993373200f94d587d113b3
chenzhe <zhe@eatigo.com>
2018-10-18 07:52:12 +0800

    finish 1017 (longest substring of the k distinct characters)

--- src/dailyproblem/Daily20181017.java
+++ src/dailyproblem/Daily20181017.java
@@ -25,12 +25,109 @@ https://leetcode.com/problems/find-k-th-smallest-pair-distance/description/
 public class Daily20181017 {
 
     public String longestSubArrayDistance(String s, int k) {
+        if (k >= s.length()) return s;
         Map<Character, Integer> chars = new HashMap<>();
+        Map<Character, Integer> charPos = new HashMap<>();
         int start = 0;
-        int end = k-1;
-        for (int i = 0 ; i < s.toCharArray().length; i++)  {
-
+        int end = 0;
+        int i = 0;
+        int count = 0;
+        while (true) {
+            if (count == k) {
+                end = i;
+                break;
+            }
+            if (charPos.containsKey(s.charAt(i))) {
+                chars.put(s.charAt(i), i - charPos.get(s.charAt(i)));
+            } else {
+                charPos.put(s.charAt(i), i);
+                chars.put(s.charAt(i), -1);
+                count++;
+            }
+            i++;
         }
         return null;
+    }
+
+    // O(n) space O(n^2) time
+    public static int lengthOfLongestSubstringTwoDistinct(String s, int k) {
+        int max = 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int start = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+
+            if (map.size() > 2) {
+                max = Math.max(max, i - start);
+
+                while (map.size() > 2) {
+                    char t = s.charAt(start);
+                    int count = map.get(t);
+                    if (count > 1) {
+                        map.put(t, count - 1);
+                    } else {
+                        map.remove(t);
+                    }
+                    start++;
+                }
+            }
+        }
+
+        max = Math.max(max, s.length() - start);
+
+        return max;
+    }
+
+    // O(n) time
+    public static int lengthOfLongestSubstringKDistinct(String s, int k) {
+        if (k == 0 || s == null || s.length() == 0)
+            return 0;
+
+        if (s.length() < k)
+            return s.length();
+
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+
+        int maxLen = k;
+        int left = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+
+            if (map.size() > k) {
+                maxLen = Math.max(maxLen, i - left);
+
+                while (map.size() > k) {
+
+                    char fc = s.charAt(left);
+                    if (map.get(fc) == 1) {
+                        map.remove(fc);
+                    } else {
+                        map.put(fc, map.get(fc) - 1);
+                    }
+
+                    left++;
+                }
+            }
+
+        }
+
+        maxLen = Math.max(maxLen, s.length() - left);
+
+        return maxLen;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(lengthOfLongestSubstringKDistinct("abcba", 2));
     }
 }
