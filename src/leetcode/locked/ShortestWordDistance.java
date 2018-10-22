29ca689f21809e21bea0c56c153cbba3a4d48ee2
chenzhe <zhe@eatigo.com>
2018-10-22 13:06:04 +0800

    add leetcode locker questions 624 Maximum Distance in Arrays 243,245 Maximum Distance in Arrays I/II/II

---
+++ src/leetcode/locked/ShortestWordDistance.java
@@ -0,0 +1,17 @@
+package leetcode.locked;
+
+/*
+leetcode 243
+https://tonycao.gitbooks.io/leetcode-locked/content/LeetCode%20Locked/c1.4.html
+Questions 1:
+Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+For example, Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+
+Given word1 = "coding", word2 = "practice", return 3. Given word1 = "makes", word2 = "coding", return 1.
+
+@duplicated with ShortestDistance
+
+ */
+public class ShortestWordDistance {
+}
