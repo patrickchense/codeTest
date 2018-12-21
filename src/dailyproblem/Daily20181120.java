d1a17b32268f9d6333aab4aa7466d4f3324f4882
chenzhe <zhe@eatigo.com>
2018-12-21 12:34:49 +0800

    solved 1120 max sell buy stocks(once)

--- src/dailyproblem/Daily20181120.java
+++ src/dailyproblem/Daily20181120.java
@@ -8,4 +8,28 @@ For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could bu
 @facebook
  */
 public class Daily20181120 {
+
+    // dp questions
+    /*
+        keep the min
+     */
+    public static int maxBuySellStock(int[] prices) {
+        int[] earns = new int[prices.length-1];
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > min) {
+                earns[i-1] = prices[i] - min;
+            } else {
+                min = prices[i];
+            }
+        }
+        int max = 0;
+        for (int i : earns) max = Math.max(i, max);
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = new int[]{9, 11, 8, 5, 7, 10};
+        System.out.println(maxBuySellStock(prices));
+    }
 }
