d1768749ca2113b983f1301365bfc4bf7c559b4e
chenzhe <zhe@eatigo.com>
2018-12-24 11:12:20 +0800

    add questions from 1209 to 1224

---
+++ src/dailyproblem/Daily20181216.java
@@ -0,0 +1,10 @@
+package dailyproblem;
+
+/*
+Given the head of a singly linked list, reverse it in-place.
+@google
+
+
+ */
+public class Daily20181216 {
+}
---
+++ src/dailyproblem/Daily20181223.java
@@ -0,0 +1,15 @@
+package dailyproblem;
+
+/*
+Given the root of a binary tree, return a deepest node. For example, in the following tree, return d.
+
+    a
+   / \
+  b   c
+ /
+d
+@google
+
+ */
+public class Daily20181223 {
+}
