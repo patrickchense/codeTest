c2b8fb34c4cb308c953e736aef49cac59afd809b
chenzhe <zhe@eatigo.com>
2018-12-24 11:34:02 +0800

    solved 1212 largest multiple amount of three in the array

--- src/dailyproblem/Daily20181212.java
+++ src/dailyproblem/Daily20181212.java
@@ -1,5 +1,7 @@
 package dailyproblem;
 
+import java.util.Arrays;
+
 /*
 Given a list of integers, return the largest product that can be made by multiplying any three integers.
 
@@ -8,6 +10,36 @@ For example, if the list is [-10, -10, 5, 2], we should return 500, since that's
 You can assume the list has at least three integers.
 
 @facebook
+@solved
  */
 public class Daily20181212 {
+
+    public static void main(String[] args){
+        int[] arr = new int[]{-10, -10, 5, 2};
+        System.out.println(largestMultipleOfThree(arr));
+        arr = new int[]{-10, 10, 5, 2};
+        System.out.println(largestMultipleOfThree(arr));
+        arr = new int[]{1, 10, 5, 2};
+        System.out.println(largestMultipleOfThree(arr));
+    }
+
+    /*
+        two ways,
+            1. two smallest negative, and largest positive
+            2. three positive
+     */
+    public static long largestMultipleOfThree(int[] arr) {
+        Arrays.sort(arr);
+        long negative = 1;
+        long positive = 1;
+        for (int i = 0; i <= 1; i ++){
+            if (arr[i] < 0) {
+                negative *= arr[i];
+            }
+        }
+        for (int i = arr.length - 1; i>= arr.length-3; i--) {
+            positive *= arr[i];
+        }
+        return Math.max(negative*arr[arr.length - 1], positive);
+    }
 }
