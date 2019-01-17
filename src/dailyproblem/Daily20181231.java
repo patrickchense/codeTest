ecf847117b0c187cfeded7b767f7f4bba5018212
chenzhe <zhe@eatigo.com>
2019-01-17 17:09:25 +0800

    solved 12.31 divide function

--- src/dailyproblem/Daily20181231.java
+++ src/dailyproblem/Daily20181231.java
@@ -5,7 +5,32 @@ Implement division of two positive integers without using the division, multipli
 
 @ContextLogic
 
-
+@solved
  */
 public class Daily20181231 {
+
+
+    public static void main(String[] args) {
+        System.out.println(divide(101, 2));
+        System.out.println(divide(101, 3));
+    }
+
+    // O(log2N)?
+    public static int divide(int a, int b) {
+        //assume a > b
+        int limit = b*2;
+        int num = 0;
+        int previous = 0;
+        while (limit < a) {
+            previous = num;
+            limit = b * num;
+        }
+        if (limit == a) return num;
+        while (num > previous) {
+            limit -= b;
+            num--;
+            if (limit <= a) break;
+        }
+        return num;
+    }
 }
