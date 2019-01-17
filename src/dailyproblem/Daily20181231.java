3236a6ea252fbaaaf1df26f69d13f7375822087c
chenzhe <zhe@eatigo.com>
2019-01-17 17:28:52 +0800

    solved 12.31 add a better way

--- src/dailyproblem/Daily20181231.java
+++ src/dailyproblem/Daily20181231.java
@@ -18,7 +18,7 @@ public class Daily20181231 {
     // O(log2N)?
     public static int divide(int a, int b) {
         //assume a > b
-        int limit = b*2;
+        int limit = b * 2;
         int num = 0;
         int previous = 0;
         while (limit < a) {
@@ -32,5 +32,43 @@ public class Daily20181231 {
             if (limit <= a) break;
         }
         return num;
+    }
+
+
+    // better !!!!! only O(log(a)) ,  space O(1)
+    public static long divide(long dividend,
+                              long divisor) {
+
+// Calculate sign of divisor
+// i.e., sign will be negative
+// only iff either one of them
+// is negative otherwise it
+// will be positive
+        long sign = ((dividend < 0) ^
+                (divisor < 0)) ? -1 : 1;
+
+// remove sign of operands
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+
+// Initialize the quotient
+        long quotient = 0, temp = 0;
+
+// test down from the highest
+// bit and accumulate the
+// tentative value for
+// valid bit
+// 1<<31 behaves incorrectly and gives Integer
+// Min Value which should not be the case, instead
+        // 1L<<31 works correctly.
+        for (int i = 31; i >= 0; --i) {
+
+            if (temp + (divisor << i) <= dividend) {
+                temp += divisor << i;
+                quotient |= 1L << i;
+            }
+        }
+
+        return (sign * quotient);
     }
 }
