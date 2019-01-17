118e8cc740285b93ae540a0853263354f29569f9
chenzhe <zhe@eatigo.com>
2019-01-17 15:14:50 +0800

    solved 12.29 parentheses match need remove howmany

--- src/dailyproblem/Daily20181229.java
+++ src/dailyproblem/Daily20181229.java
@@ -6,7 +6,32 @@ Given a string of parentheses, write a function to compute the minimum number of
 For example, given the string "()())()", you should return 1. Given the string ")(", you should return 2, since we must remove all of them.
 
 @google
-
+@solved
  */
 public class Daily20181229 {
+
+
+    public static void main(String[] args) {
+        System.out.println(RemoveParenthesesNumber("()())()"));
+        System.out.println(RemoveParenthesesNumber(")("));
+        System.out.println(RemoveParenthesesNumber(")()((())))"));
+    }
+
+    public static int RemoveParenthesesNumber(String s) {
+        int num = 0;
+        int bal = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') bal++;
+            else {
+                if (bal == 0) {
+                    // not enough
+                    num++;
+                    continue;
+                } else {
+                    bal--;
+                }
+            }
+        }
+        return num + bal;
+    }
 }
