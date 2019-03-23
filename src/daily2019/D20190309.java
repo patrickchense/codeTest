9cded7cc540edcbadc72dc175cd34cb7845e76ce
chenzhe <zhe@eatigo.com>
2019-03-23 09:31:15 +0800

    add 20190301-20190311

---
+++ src/daily2019/D20190309.java
@@ -0,0 +1,14 @@
+package daily2019;
+
+/*
+Given a positive integer n, find the smallest number of squared integers which sum to n.
+
+For example, given n = 13, return 2 since 13 = 32 + 22 = 9 + 4.
+
+Given n = 27, return 3 since 27 = 32 + 32 + 32 = 9 + 9 + 9.
+
+@Facebook
+
+ */
+public class D20190309 {
+}
---
+++ src/daily2019/D20190310.java
@@ -0,0 +1,12 @@
+package daily2019;
+
+/*
+Given a string, determine whether any permutation of it is a palindrome.
+
+For example, carrace should return true, since it can be rearranged to form racecar, which is a palindrome. daily should return false, since there's no rearrangement that can form a palindrome.
+
+@Amazon
+
+ */
+public class D20190310 {
+}
