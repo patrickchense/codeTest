1d7e9dbab0be45e73c77c2a680becd5f79ed9565
chenzhe <zhe@eatigo.com>
2019-03-23 21:09:07 +0800

    solved smallest distance between two words in string arr (特殊的算法)

--- src/daily2019/D20190306.java
+++ src/daily2019/D20190306.java
@@ -3,8 +3,48 @@ package daily2019;
 /*
 Find an efficient algorithm to find the smallest distance (measured in number of words) between any two given words in a string.
 
-For example, given words "hello", and "world" and a text content of "dog cat hello cat dog dog hello cat world", return 1 because there's only one word "cat" in between the two words.
+For example, given words "hello", and "world" and a text content of "dog cat hello cat dog dog hello cat world",
+return 1 because there's only one word "cat" in between the two words.
 
+@solved
  */
 public class D20190306 {
+    public static void main(String[] args) {
+        System.out.println(smallestDistanceWords("hello", "world", "dog cat hello cat dog dog hello cat world"));
+        System.out.println(smallestDistanceWords("hello", "world", "dog cat hello cat dog dog hello cat world hello"));
+        System.out.println(smallestDistanceWords("hello", "world", "dog cat world cat dog dog hello cat world d f hello"));
+    }
+
+    // 碰到str1, 记录，下次碰到str1, 则更新index，因为后面的不会更近
+    // 碰到str2, 计算，然后保存，下次碰到str2, 记录index,
+    // 碰到str1, 计算，比较如果小，替换，
+    public static int smallestDistanceWords(String str1, String str2, String str) {
+        int distance = Integer.MAX_VALUE;
+        int str1_i = -1;
+        int str2_i = -1;
+        boolean str1_before = false;
+        boolean str2_before = false;
+        String strs[] = str.split(" ");
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].equals(str1)) {
+                if (str2_before) {
+                    distance = Math.min(distance, i - str2_i);
+                    str2_before = false;
+                    str2_i = -1;
+                }
+                str1_i = i;
+                str1_before = true;
+            }
+            else if (strs[i].equals(str2)) {
+                if (str1_before) {
+                    distance = Math.min(distance, i - str1_i);
+                    str1_before = false;
+                    str1_i = -1;
+                }
+                str2_i = i;
+                str2_before = true;
+            }
+        }
+        return distance -1;
+    }
 }
