1ef8177d14b163409afb9c3e2d9d3b3dc97ebc36
chenzhe <zhe@eatigo.com>
2019-03-24 12:07:06 +0800

    solved nearest larger elem

--- src/daily2019/D20190225.java
+++ src/daily2019/D20190225.java
@@ -13,4 +13,37 @@ Follow-up: If you can preprocess the array, can you do this in constant time?
 
  */
 public class D20190225 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{4, 1, 3, 5, 6};
+        System.out.println(nearestLargerDistance(0, arr));
+        System.out.println(nearestLargerDistance(1, arr));
+        System.out.println(nearestLargerDistance(2, arr));
+        System.out.println(nearestLargerDistance(3, arr));
+        System.out.println(nearestLargerDistance(4, arr));
+    }
+
+    // O(n)
+    public static Integer nearestLargerDistance(int original, int[] arr) {
+        int n = arr[original];
+        int nearIndex = Integer.MAX_VALUE;
+        for(int i = original+1; i < arr.length; i++) {
+            if (arr[i] > n) {
+                nearIndex = i - original;
+                break;
+            }
+        }
+        int nearIndexLeft = Integer.MAX_VALUE;
+        for (int i = original -1; i >= 0; i--) {
+            if (arr[i] > n) {
+                nearIndexLeft = original -i;
+                break;
+            }
+        }
+        if (nearIndex == Integer.MAX_VALUE && nearIndexLeft == Integer.MAX_VALUE) return null;
+        return Math.min(nearIndex, nearIndexLeft);
+    }
+
+    // how O(1) ? sorted? always return 1
+    // hashmap?
 }
