26fd5da710aeaf1644b3db67416d1307dac3da5a
chenzhe <zhe@eatigo.com>
2019-03-25 22:09:58 +0800

    code

--- src/daily2019/D20190201.java
+++ src/daily2019/D20190201.java
@@ -1,11 +1,14 @@
 package daily2019;
 
 /*
-Implement the singleton pattern with a twist. First, instead of storing one instance, store two instances. And in every even call of getInstance(), return the first instance and in every odd call of getInstance(), return the second instance.
+Implement the singleton pattern with a twist.
+First, instead of storing one instance, store two instances.
+And in every even call of getInstance(), return the first instance and in every odd call of getInstance(), return the second instance.
 
 @Microsoft
-
+@design
 
  */
 public class D20190201 {
+
 }
--- src/daily2019/D20190202.java
+++ src/daily2019/D20190202.java
@@ -6,7 +6,18 @@ Given a string which we can delete at most k, return whether you can make a pali
 For example, given 'waterrfetawx' and a k of 2, you could delete f and x to get 'waterretaw'.
 
 @Google
+@string
+@palindrome
+
+这题从中间开始，length == odd mid-1 mid+1， even  mid, mid+1开始,
+如果不等 找是否有相等，
 
  */
 public class D20190202 {
+
+    public static void main(String[] args) {
+
+    }
+
+
 }
