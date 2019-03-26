93d395090185d61bce89c0c88a3f6c9c854b6599
chenzhe <zhe@eatigo.com>
2019-03-26 22:07:33 +0800

    not finish 0126

--- src/daily2019/D20190126.java
+++ src/daily2019/D20190126.java
@@ -1,11 +1,33 @@
 package daily2019;
 
 /*
-Given a string and a set of delimiters, reverse the words in the string while maintaining the relative order of the delimiters. For example, given "hello/world:here", return "here/world:hello"
+Given a string and a set of delimiters, reverse the words in the string while maintaining the relative order of the delimiters.
+For example, given "hello/world:here", return "here/world:hello"
 
 Follow-up: Does your solution work for the following cases: "hello/world:here/", "hello//world:here"
 
 @Facebook
  */
 public class D20190126 {
+
+    public static void main(String[] args) {
+        System.out.println(reverseDelimiters("hello/world:here"));
+        System.out.println(reverseDelimiters("hello/world:here/"));
+        System.out.println(reverseDelimiters("hello//world:here"));
+    }
+
+    // 用额外空间就很好搞定了List，不用怎么处理？
+    public static String reverseDelimiters(String str) {
+        char[] arr = new char[str.length()];
+        int i = 0;
+        int j = str.length() -1;
+        int k = 0;
+        int begin =0;
+        int end =j;
+        while (i < j) {
+            while (str.charAt(i) != '/' && str.charAt(i) != ':') i++;
+            while (str.charAt(j) != '/' && str.charAt(j) != ':') j--;
+
+        }
+    }
 }
