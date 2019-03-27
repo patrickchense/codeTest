41b3bc4473b04523fa87b854e24f2ae351a852b5
chenzhe <zhe@eatigo.com>
2019-03-28 07:38:50 +0800

    add 20190326-28

---
+++ src/daily2019/D20190327.java
@@ -0,0 +1,14 @@
+package daily2019;
+
+/*
+Describe and give an example of each of the following types of polymorphism:
+
+Ad-hoc polymorphism
+Parametric polymorphism
+Subtype polymorphism
+
+@Microsoft
+
+ */
+public class D20190327 {
+}
