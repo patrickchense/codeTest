35ecadc84f6b6b13765f2febf3245fc916f9a0e3
chenzhe <zhe@eatigo.com>
2019-03-27 13:05:07 +0800

    solved answered 字符串reverse words, 分隔符，in-place reverse等

--- src/daily2019/D20190125.java
+++ src/daily2019/D20190125.java
@@ -6,6 +6,65 @@ Given a string of words delimited by spaces, reverse the words in string. For ex
 Follow-up: given a mutable string representation, can you perform this operation in-place?
 
 @Google
+@string
+@reverse
+@answered
+@review
+
+跟0126是一个题目
+https://www.geeksforgeeks.org/reverse-words-given-string-java/
+
  */
 public class D20190125 {
+
+    public static void main(String[] args) {
+        System.out.println(reverseWords("hello world here"));
+    }
+
+
+    public static String reverseWords(String str) {
+        String[] words = str.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            if (i == words.length - 1) {
+                sb.insert(0, words[i]);
+            }
+            else {
+                sb.insert(0, " ");
+                sb.insert(1, words[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+    //in place?? 怎么做？ https://www.programcreek.com/2014/05/leetcode-reverse-words-in-a-string-ii-java/
+    /*
+    其实很简单的逻辑，
+    先reverse每个单词， hello world here -> olleh dlrow ereh
+    在reverse整个字符串, olleh dlrow ereh -> here world hello
+
+     */
+    public static void reverseWords(char[] s) {
+        int i=0;
+        for(int j=0; j<s.length; j++){
+            if(s[j]==' '){
+                reverse(s, i, j-1);
+                i=j+1;
+            }
+        }
+
+        reverse(s, i, s.length-1);
+
+        reverse(s, 0, s.length-1);
+    }
+
+    public static void reverse(char[] s, int i, int j){
+        while(i<j){
+            char temp = s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        }
+    }
 }
--- src/daily2019/D20190126.java
+++ src/daily2019/D20190126.java
@@ -1,5 +1,8 @@
 package daily2019;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 Given a string and a set of delimiters, reverse the words in the string while maintaining the relative order of the delimiters.
 For example, given "hello/world:here", return "here/world:hello"
@@ -7,27 +10,87 @@ For example, given "hello/world:here", return "here/world:hello"
 Follow-up: Does your solution work for the following cases: "hello/world:here/", "hello//world:here"
 
 @Facebook
+@solved
+
  */
 public class D20190126 {
 
     public static void main(String[] args) {
-        System.out.println(reverseDelimiters("hello/world:here"));
-        System.out.println(reverseDelimiters("hello/world:here/"));
-        System.out.println(reverseDelimiters("hello//world:here"));
+        System.out.println(reverseWords("hello/world:here"));
+        System.out.println(reverseWords("hello/world:here/"));
+        System.out.println(reverseWords("hello//world:here"));
     }
 
-    // 用额外空间就很好搞定了List，不用怎么处理？
-    public static String reverseDelimiters(String str) {
-        char[] arr = new char[str.length()];
-        int i = 0;
-        int j = str.length() -1;
-        int k = 0;
-        int begin =0;
-        int end =j;
-        while (i < j) {
-            while (str.charAt(i) != '/' && str.charAt(i) != ':') i++;
-            while (str.charAt(j) != '/' && str.charAt(j) != ':') j--;
+    // 直接写也不容易，要注意的好几个的地方
+    /*
+    1. 怎么处理多个delimiters连在一起
+    2. 处理最后的字符串没有加入到sb中
+    3.
+     */
+    public static String reverseWords(String str) {
+        List<String> strs = new ArrayList<>();
+        int j = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '/' || str.charAt(i) == ':') {
+                if (j == i ) {
+                    j = i + 1;
+                    continue;
+                }
+                strs.add(str.substring(j, i));
+                j = i + 1;
+            }
+        }
+        if (j < str.length()) {
+            strs.add(str.substring(j));
+        }
+        StringBuilder sb = new StringBuilder();
+        j = strs.size();
+        int last = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '/' || str.charAt(i) == ':') {
+                if (i != 0 && i - 1 != last) {  // 关键用last判断是不是delimiters连续
+                    j--;
+                    sb.append(strs.get(j));
 
+                }
+                sb.append(str.charAt(i));
+                last = i;
+            }
+        }
+        if (j > 0) {  //处理可能剩下的一个
+            sb.append(strs.get(0));
+        }
+        return sb.toString();
+    }
+
+
+    // 用额外空间就很好搞定了String split，不用怎么处理？
+    public static String reverseDelimiters(String str) {
+        char[] cs = str.toCharArray();
+        int j = 0;
+        for (int i = 0; i < cs.length; i++) {
+            if (cs[i] == '/' || cs[i] == ':') {
+                reverse(cs, j, i-1);
+                j = i + 1;
+            }
+        }
+        reverse(cs, j, cs.length - 1);
+        System.out.println(new String(cs));
+        // olleh/dlrow:ereh
+        // here:world/hello
+        //
+        reverse(cs, 0, cs.length - 1); //这里有问题，因为标号不同，不能替换了，必须想其他办法
+
+        return new String(cs);
+    }
+
+    public static void reverse(char[] s, int i, int j){
+        while(i<j){
+            char temp = s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
         }
     }
 }
