a366fb9ef90743815c9ea6e52e8e23c54fc42fa5
chenzhe <zhe@eatigo.com>
2019-03-29 20:34:33 +0800

    solved dp solution to unsorted array longest consecutive numbers

--- src/daily2019/D20190111.java
+++ src/daily2019/D20190111.java
@@ -1,5 +1,7 @@
 package daily2019;
 
+import java.util.Arrays;
+
 /*
 Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
 
@@ -8,7 +10,35 @@ For example, given [100, 4, 200, 1, 3, 2], the longest consecutive element seque
 Your algorithm should run in O(n) complexity.
 
 @microsoft
+@solved
+@dp
+@array
+@sort
 
  */
 public class D20190111 {
+    public static void main(String[] args) {
+        System.out.println(longestConsecutiveNumberLen(new int[]{100, 4, 200, 1, 3, 2}));
+    }
+
+    /*
+    un sorted ?
+    consecutive?
+    第一种能想到的， sorted, 然后dp
+     */
+    public static int longestConsecutiveNumberLen(int[] arr) {
+        Arrays.sort(arr);
+        int[] lens = new int[arr.length];
+        lens[0] = 1;
+        int max = 1;
+        for (int i = 1; i < arr.length; i++) {
+            // dp formula L[i] = L[i-1] if arr[i] != a[i-1] + 1, else L[i] = L[i-1] + 1;
+            if (arr[i] == arr[i-1] + 1) {
+                lens[i] = lens[i-1] + 1;
+                max = Math.max(lens[i], max);
+            }
+            else lens[i] = lens[i-1];
+        }
+        return max;
+    }
 }
