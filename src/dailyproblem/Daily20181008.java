477908716caa6c6660de543fd9852a1a0d79a5fe
chenzhe <zhe@eatigo.com>
2019-04-05 15:28:07 +0800

    solved lowest missing positive number in array (O(n) O(1))

--- src/dailyproblem/Daily20181008.java
+++ src/dailyproblem/Daily20181008.java
@@ -8,6 +8,13 @@ For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should g
 
 You can modify the input array in-place.
 
+@Stripe
+@array
+@solved
+@optimize 最优解O(n) O(1)
+@classic
+
+这题用了一个很经典的手段 a[a[i]] 的方式来modify inplace
  */
 public class Daily20181008 {
 
@@ -22,11 +29,21 @@ public class Daily20181008 {
     }
 
     /*
-        if minPositive - nums[i] > 1,  missingPositive = nums[i] + 1;
-
+        直接a[a[i]] 修改值， 如果a[i] >= a.length, 直接抛弃，如果a[i] < 0 抛弃
+        [0] = 1, [1] = 0, [2] = 1, [3] = 1
+        [0] = 1, [1] = 1, [2] = 0,
+        如果没有，那么就是a.length
 
      */
     private static int findMissingPositive(int[] nums) {
-        return -1;
+        for (int i : nums) {
+            if (i > nums.length || i <= 0) continue;
+            nums[i-1] = 1;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) continue;
+            return i + 1;
+        }
+        return nums.length;
     }
 }
