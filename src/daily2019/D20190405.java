22bc086ecbb788e00d24dc7b91d2c7e9eeaf0b2e
chenzhe <zhe@eatigo.com>
2019-04-06 11:55:47 +0800

    solved gcd in array

---
+++ src/daily2019/D20190405.java
@@ -0,0 +1,72 @@
+package daily2019;
+
+/*
+Given n numbers, find the greatest common denominator between them.
+
+For example, given the numbers [42, 56, 14], return 14.
+
+@Amazon
+@math
+@array
+@solved
+最大公约数
+ */
+public class D20190405 {
+
+    public static void main(String[] args) {
+        System.out.println(maxDenomiator(new int[]{42, 56, 14}));
+        System.out.println(maxDenomiator(new int[]{42, 56, 16}));
+    }
+
+    /*
+    找到最小，然后最小的约数去试
+    不知道是不是最好的方式，现在 O(min * n),
+     */
+    public static int maxDenomiator(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            min = Math.min(arr[i], min);
+        }
+        int t = min;
+        while(t > 1) {
+            if (min % t == 0) {
+                boolean isAll = true;
+                for (int i : arr) {
+                    if ( i % t != 0) {
+                        isAll = false;
+                        break;
+                    }
+                }
+                if (isAll) return t;
+            }
+            t--;
+        }
+        return 1;
+    }
+    // https://www.geeksforgeeks.org/gcd-two-array-numbers/
+    /*
+    The GCD of three or more numbers equals the product of the prime factors common to all the numbers, but it can also be calculated by
+    repeatedly taking the GCDs of pairs of numbers.
+
+    result = arr[0]
+For i = 1 to n-1
+   result = GCD(result, arr[i])
+     */
+    static int gcd(int a, int b)
+    {
+        if (a == 0)
+            return b;
+        return gcd(b % a, a);
+    }
+
+    // Function to find gcd of array of
+    // numbers
+    static int findGCD(int arr[], int n)
+    {
+        int result = arr[0];
+        for (int i = 1; i < n; i++)
+            result = gcd(arr[i], result);
+
+        return result;
+    }
+}
