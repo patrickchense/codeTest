4f4533ed3452f5079f64d801fa684d45001aab09
chenzhe <zhe@eatigo.com>
2019-04-07 12:43:47 +0800

    solved substring dictionary match answered rain filled up count

--- src/dailyproblem/Daily20181026.java
+++ src/dailyproblem/Daily20181026.java
@@ -1,15 +1,54 @@
 package dailyproblem;
 
+import util.ArrayUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 
-Given a dictionary of words and a string made up of those words (no spaces), return the original sentence in a list. If there is more than one possible reconstruction, return any of them. If there is no possible reconstruction, then return null.
+Given a dictionary of words and a string made up of those words (no spaces),
+ return the original sentence in a list. If there is more than one possible reconstruction, return any of them. If there is no possible reconstruction, then return null.
 
 For example, given the set of words 'quick', 'brown', 'the', 'fox', and the string "thequickbrownfox", you should return ['the', 'quick', 'brown', 'fox'].
 
 Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the string "bedbathandbeyond", return either ['bed', 'bath', 'and', 'beyond] or ['bedbath', 'and', 'beyond']
 
 @Microsoft
+@string
+@substring
+@sovled
+
+感觉可以优化，没有最优
 
  */
 public class Daily20181026 {
+
+    public static void main(String[] args) {
+        ArrayUtil.printList(findStrInDic(new String[]{"quick", "brown", "the", "fox"}, "thequickbrownfox"));
+        ArrayUtil.printList(findStrInDic(new String[]{"bed", "bath", "bedbath", "beyond", "and"}, "bedbathandbeyond"));
+    }
+
+    // O(n*k) ?
+    public static List<String> findStrInDic(String[] dic, String str) {
+        List<String> res = new ArrayList<>();
+        int i = 0;
+        boolean isFinished = false;
+        while(!isFinished) {
+            int t = i;
+            for (String d : dic) {
+                int j = i + d.length();
+                if (j <= str.length() && str.substring(i, j).equals(d)) {
+                    res.add(d);
+                    i = j;
+                    if (j == str.length()) {
+                        isFinished = true;
+                        break;
+                    }
+                }
+            }
+            if (t == i) break; // 代表没有一个dicmatch，直接退出
+        }
+        return res.isEmpty() || !isFinished ? null : res;
+    }
 }
--- src/dailyproblem/Daily20181103.java
+++ src/dailyproblem/Daily20181103.java
@@ -12,27 +12,116 @@ Given the input [3, 0, 1, 3, 0, 5], we can hold 3 units in the first index, 2 in
  (we cannot hold 5 since it would run off to the left), so we can trap 8 units of water.
 
 @facebook
+@array
+@dp
+@answered
+@review
+
+https://www.geeksforgeeks.org/trapping-rain-water/
+
+不会
+
+related:
+https://www.geeksforgeeks.org/maximum-litres-of-water-that-can-be-bought-with-n-rupees/
+https://www.geeksforgeeks.org/amazon-pune-pool-campus-interview/
+https://www.geeksforgeeks.org/find-maximum-sum-taking-every-kth-element-in-the-array/
+https://www.geeksforgeeks.org/find-the-minimum-number-of-operations-required-to-make-all-array-elements-equal/
 
 
  */
 public class Daily20181103 {
 
+    public static void main(String[] args) {
+        System.out.println(findWater(new int[]{2,1,2}, 3));
+        System.out.println(findWater(new int[]{3, 0, 1, 3, 0, 5}, 6));
+        System.out.println(findWaterSpaceOptimize(new int[]{2,1,2}, 3));
+        System.out.println(findWaterSpaceOptimize(new int[]{3, 0, 1, 3, 0, 5}, 6));
+    }
+
+
+    // O(n) O(n)
+    //An Efficient Solution is to prre-compute highest bar on left and right of every bar in O(n) time. Then use these pre-computed values to find the amount of water in every array element
+    static int findWater(int[] arr, int n)
+    {
+        // left[i] contains height of tallest bar to the
+        // left of i'th bar including itself
+        int left[] = new int[n];
+
+        // Right [i] contains height of tallest bar to
+        // the right of ith bar including itself
+        int right[] = new int[n];
+
+        // Initialize result
+        int water = 0;
+
+        // Fill left array
+        left[0] = arr[0];
+        for (int i = 1; i < n; i++)
+            left[i] = Math.max(left[i-1], arr[i]);
+
+        // Fill right array
+        right[n-1] = arr[n-1];
+        for (int i = n-2; i >= 0; i--)
+            right[i] = Math.max(right[i+1], arr[i]);
+
+        // Calculate the accumulated water element by element
+        // consider the amount of water on i'th bar, the
+        // amount of water accumulated on this particular
+        // bar will be equal to min(left[i], right[i]) - arr[i] .
+        for (int i = 0; i < n; i++)
+            water += Math.min(left[i],right[i]) - arr[i];
+
+        return water;
+    }
+
     /*
-    find the one >= index[0]
-        count += index[0] - index[i]
-        wall_index = i
+    Space Optimization in above solution :
+Instead of maintaing two arrays of size n for storing left and right max of each element,
+we will just maintain two variables to store the maximum till that point. Since water trapped at
+ any element = min( max_left, max_right) – arr[i] we will calculate water trapped on smaller element out of A[lo]
+ and A[hi] first and move the pointers till lo doesn’t cross hi.
 
-        2, 1, 2  =>  lagest two is 2,2
-        3, 0, 1, 3, 0, 5] => 3, 3, 5
-        3, 4, 1, 5, 0, 3 =>  3, 4, 5, 3
-        5, 0, 3, 2, 1 =>
+ 用类似2指针的方式，找左边和右边的最大，然后统计
      */
-    public static int holdUnitWater(int[] nums) {
-        int unit = 0;
-        int wallIndex = 0;
-        for (int i = 1; i < nums.length; i++) {
+    static int findWaterSpaceOptimize(int arr[], int n)
+    {
+        // initialize output
+        int result = 0;
 
+        // maximum element on left and right
+        int left_max = 0, right_max = 0;
+
+        // indices to traverse the array
+        int lo = 0, hi = n-1;
+
+        while(lo <= hi)
+        {
+            if(arr[lo] < arr[hi])
+            {
+                if(arr[lo] > left_max)
+
+                    // update max in left
+                    left_max = arr[lo];
+                else
+
+                    // water on curr element =
+                    // max - curr
+                    result += left_max - arr[lo];
+                lo++;
+            }
+            else
+            {
+                if(arr[hi] > right_max)
+
+                    // update right maximum
+                    right_max = arr[hi];
+
+                else
+                    result += right_max - arr[hi];
+                hi--;
+            }
         }
-        return unit;
+
+        return result;
     }
 }
