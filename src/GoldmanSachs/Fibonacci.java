200e7bcdac60d6ef03bfafd30e9186b96cdb798f
chenzhe <zhe@eatigo.com>
2019-04-09 09:59:14 +0800

    answered random shuffle in-place to array

--- src/GoldmanSachs/Fibonacci.java
+++ src/GoldmanSachs/Fibonacci.java
@@ -89,4 +89,80 @@ public class Fibonacci {
         }
         return b;
     }
+
+    // O(logn) O(1)
+    //https://www.geeksforgeeks.org/space-efficient-iterative-method-fibonacci-number-set-2/
+    // get second MSB
+    static int getMSB(int n)
+    {
+        // consectutively set
+        // all the bits
+        n |= n >> 1;
+        n |= n >> 2;
+        n |= n >> 4;
+        n |= n >> 8;
+        n |= n >> 16;
+
+        // returns the
+        // second MSB
+        return ((n + 1) >> 2);
+    }
+
+    // Multiply function
+    static void multiply(int F[][],
+                         int M[][])
+    {
+        int x = F[0][0] * M[0][0] +
+                F[0][1] * M[1][0];
+        int y = F[0][0] * M[0][1] +
+                F[0][1] * M[1][1];
+        int z = F[1][0] * M[0][0] +
+                F[1][1] * M[1][0];
+        int w = F[1][0] * M[0][1] +
+                F[1][1] * M[1][1];
+
+        F[0][0] = x;
+        F[0][1] = y;
+        F[1][0] = z;
+        F[1][1] = w;
+    }
+
+    // Function to calculate F[][]
+// raise to the power n
+    static void power(int F[][],
+                      int n)
+    {
+        // Base case
+        if (n == 0 || n == 1)
+            return;
+
+        // take 2D array to
+        // store number's
+        int[][] M ={{1, 1},
+                {1, 0}};
+
+        // run loop till MSB > 0
+        for (int m = getMSB(n);
+             m > 0; m = m >> 1)
+        {
+            multiply(F, F);
+
+            if ((n & m) > 0)
+            {
+                multiply(F, M);
+            }
+        }
+    }
+
+    // To return
+// fibonacci numebr
+    static int fib(int n)
+    {
+        int[][] F = {{1, 1},
+                {1, 0}};
+        if (n == 0)
+            return 0;
+        power(F, n - 1);
+        return F[0][0];
+    }
 }
--- src/dailyproblem/Daily20181124.java
+++ src/dailyproblem/Daily20181124.java
@@ -1,5 +1,10 @@
 package dailyproblem;
 
+import util.ArrayUtil;
+
+import java.util.Arrays;
+import java.util.Random;
+
 /*
 Given a function that generates perfectly random numbers between 1 and k (inclusive), where k is an input, write a function that shuffles a deck of cards represented as an array using only swaps.
 
@@ -7,8 +12,48 @@ It should run in O(N) time.
 
 Hint: Make sure each one of the 52! permutations of the deck is equally likely.
 
-@facebook
+@Facebook
+@review
+@answered
+@random
+
+第一次做这种random题，没经验
+https://www.geeksforgeeks.org/select-a-random-number-from-stream-with-o1-space
+
+
+这题应该相似，还是利用了Random类
+https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/
+
+Let the given array be arr[]. A simple solution is to create an auxiliary array temp[] which is initially a copy of arr[]. Randomly select an element from temp[],
+ copy the randomly selected element to arr[0] and remove the selected element from temp[]. Repeat the same process n times and keep copying elements
+  to arr[1], arr[2], … . The time complexity of this solution will be O(n^2).
+
+Fisher–Yates shuffle Algorithm works in O(n) time complexity. The assumption here is, we are given a function rand() that generates random number in O(1) time.
+The idea is to start from the last element, swap it with a randomly selected element from the whole array (including last). Now consider the array from 0 to n-2 (size reduced by 1), and repeat the process till we hit the first element.
+
+Following is the detailed algorithm
+
+To shuffle an array a of n elements (indices 0..n-1):
+  for i from n - 1 downto 1 do
+       j = random integer with 0 <= j <= i
+       exchange a[j] and a[i]
 
  */
 public class Daily20181124 {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(randomize(new Integer[]{1,2,3,4,5,6,7,8,9,10}, 10)));
+        System.out.println(Arrays.toString(randomize(new Integer[]{1,2,3,4,5,6,7,8,9,10}, 10)));
+    }
+
+    public static Integer[] randomize(Integer[] arr, int k) {
+        Random r = new Random();
+        for (int i = k - 1; i >0; i--) {
+            int j =  r.nextInt(i+1);
+
+            ArrayUtil.swap(arr, i, j);
+        }
+        return arr;
+    }
+
 }
