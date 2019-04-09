73e65dae8336c9ebcf64ade4bdaec775d9ed9ca4
chenzhe <zhe@eatigo.com>
2019-04-10 07:37:46 +0800

    add 20190409

---
+++ src/daily2019/D20190409.java
@@ -0,0 +1,26 @@
+package daily2019;
+
+/*
+What will this code print out?
+
+def make_functions():
+    flist = []
+
+    for i in [1, 2, 3]:
+        def print_i():
+            print(i)
+        flist.append(print_i)
+
+    return flist
+
+functions = make_functions()
+for f in functions:
+    f()
+How can we make it print out what we apparently want?
+
+@Google
+
+
+ */
+public class D20190409 {
+}
