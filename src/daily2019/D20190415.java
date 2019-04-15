199beb3e96e99ffd77100b7fa0e490d1bb092f87
chenzhe <zhe@eatigo.com>
2019-04-16 07:29:06 +0800

    add 20190415

---
+++ src/daily2019/D20190415.java
@@ -0,0 +1,12 @@
+package daily2019;
+
+/*
+Suppose you are given two lists of n points, one list p1, p2, ..., pn on the line y = 0 and the other list q1, q2, ..., qn on the line y = 1. Imagine a set of n line segments connecting each point pi to qi. Write an algorithm to determine how many pairs of the line segments intersect.
+
+@easy
+@Facebook
+
+
+ */
+public class D20190415 {
+}
