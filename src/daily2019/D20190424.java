7f453fea789fb68bbbeef5d3f67bff144ba2da39
chenzhe <zhe@eatigo.com>
2019-05-02 11:46:23 +0800

    solved binary search min in rotate array

--- src/daily2019/D20190424.java
+++ src/daily2019/D20190424.java
@@ -7,7 +7,32 @@ For example, given [5, 7, 10, 3, 4], return 3.
 
 @Uber
 @medium
+@rotate
+@array
+@findmin
+@ordered
+@binarysearch
 
+@solved
+@10min
  */
 public class D20190424 {
+
+    public static void main(String[] args) {
+        System.out.println(findMinium(new int[]{5,7,10,3,4}, 0, 4));
+        System.out.println(findMinium(new int[]{7,8,9,10,3,4,5,6}, 0, 7));
+    }
+
+    // 变形的binary search
+    public static int findMinium(int[] nums, int i, int j) {
+        if (j -i == 1) return Math.min(nums[i], nums[j]);
+        if (j == i) return nums[i];
+        int mid = (i + j) >> 1;
+        if (nums[mid] > nums[j]) {
+            return findMinium(nums, mid+1, j);
+        }
+        else {
+            return findMinium(nums, i, mid - 1);
+        }
+    }
 }
