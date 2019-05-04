ca99dc5f82fbc3f18cc0f91dea6478ade59ba07c
chenzhe <zhe@eatigo.com>
2019-05-04 11:23:34 +0800

    LT TimeBasedKVStore answered

---
+++ src/leetcode/hashtable/LT981_Time_Based_KV_Store.java
@@ -0,0 +1,103 @@
+package leetcode.hashtable;
+
+import javafx.util.Pair;
+
+import java.util.*;
+
+/*
+https://leetcode.com/problems/time-based-key-value-store/
+
+Create a timebased key-value store class TimeMap, that supports two operations.
+
+1. set(string key, string value, int timestamp)
+
+Stores the key and value, along with the given timestamp.
+2. get(string key, int timestamp)
+
+Returns a value such that set(key, value, timestamp_prev) was called previously, with timestamp_prev <= timestamp.
+If there are multiple such values, it returns the one with the largest timestamp_prev.
+If there are no values, it returns the empty string ("").
+
+
+Example 1:
+
+Input: inputs = ["TimeMap","set","get","get","set","get","get"], inputs = [[],["foo","bar",1],["foo",1],["foo",3],["foo","bar2",4],["foo",4],["foo",5]]
+Output: [null,null,"bar","bar",null,"bar2","bar2"]
+Explanation:
+TimeMap kv;
+kv.set("foo", "bar", 1); // store the key "foo" and value "bar" along with timestamp = 1
+kv.get("foo", 1);  // output "bar"
+kv.get("foo", 3); // output "bar" since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 ie "bar"
+kv.set("foo", "bar2", 4);
+kv.get("foo", 4); // output "bar2"
+kv.get("foo", 5); //output "bar2"
+
+Example 2:
+
+Input: inputs = ["TimeMap","set","set","get","get","get","get","get"], inputs = [[],["love","high",10],["love","low",20],["love",5],["love",10],["love",15],["love",20],["love",25]]
+Output: [null,null,null,"","high","high","low","low"]
+
+
+@medium
+@treemap
+@hashmap
+@binarysearch
+
+@answered
+
+两种方式，要解决get的时候查找的问题，1. treemap 2. binarysearch
+treemap 用 tree.floorKey(timestamp);
+
+
+ */
+public class LT981_Time_Based_KV_Store {
+    Map<String, List<Pair<Integer, String>>> M = new HashMap();;
+
+
+    public void set(String key, String value, int timestamp) {
+        if (!M.containsKey(key))
+            M.put(key, new ArrayList<Pair<Integer, String>>());
+
+        M.get(key).add(new Pair(timestamp, value));
+    }
+
+    public String get(String key, int timestamp) {
+        if (!M.containsKey(key)) return "";
+
+        List<Pair<Integer, String>> A = M.get(key);
+        int i = Collections.binarySearch(A, new Pair<Integer, String>(timestamp, "{"),
+                (a, b) -> Integer.compare(a.getKey(), b.getKey()));
+
+        if (i >= 0)
+            return A.get(i).getValue();
+        else if (i == -1)
+            return "";
+        else
+            return A.get(-i-2).getValue();
+    }
+
+
+}
+
+class TimeMap {
+    Map<String, TreeMap<Integer, String>> M;
+
+    public TimeMap() {
+        M = new HashMap();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        if (!M.containsKey(key))
+            M.put(key, new TreeMap());
+
+        M.get(key).put(timestamp, value);
+    }
+
+    public String get(String key, int timestamp) {
+        if (!M.containsKey(key)) return "";
+
+        TreeMap<Integer, String> tree = M.get(key);
+        Integer t = tree.floorKey(timestamp);
+        return t != null ? tree.get(t) : "";
+    }
+}
