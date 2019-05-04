b3923a0b4bf4f863865417a638519be95742e22e
chenzhe <zhe@eatigo.com>
2019-05-04 15:24:50 +0800

    LT continuous subarray sum == k

---
+++ src/leetcode/hashtable/LT560_Continuous_Subarray_Sum_K.java
@@ -0,0 +1,60 @@
+package leetcode.hashtable;
+
+/*
+https://leetcode.com/problems/subarray-sum-equals-k/
+
+Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+Example 1:
+Input:nums = [1,1,1], k = 2
+Output: 2
+Note:
+The length of the array is in range [1, 20,000].
+The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+
+@medium
+@array
+@subarray
+
+@hashmap
+@dp
+
+@answered
+
+
+ */
+
+import java.util.*;
+
+public class LT560_Continuous_Subarray_Sum_K {
+
+    // O(n) O(n)
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+
+    // O(n^2) O(1)
+    public int subarraySum2(int[] nums, int k) {
+        int count = 0;
+        int[] sum = new int[nums.length + 1];
+        sum[0] = 0;
+        for (int i = 1; i <= nums.length; i++)
+            sum[i] = sum[i - 1] + nums[i - 1];
+        for (int start = 0; start < nums.length; start++) {
+            for (int end = start + 1; end <= nums.length; end++) {
+                if (sum[end] - sum[start] == k)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
