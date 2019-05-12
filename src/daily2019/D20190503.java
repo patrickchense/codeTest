faf8f84d70fad8de27090cf5abb61d412bfde330
chenzhe <zhe@eatigo.com>
2019-05-12 07:10:41 +0200

    solved daily work

--- src/daily2019/D20190503.java
+++ src/daily2019/D20190503.java
@@ -9,6 +9,26 @@ Given a column number, return its alphabetical column id. For example, given 1, 
 
 @easy
 
+@solved
+
+@5min
  */
 public class D20190503 {
+    public static void main(String[] args) {
+        System.out.println(columId(27));
+        System.out.println(columId(10));
+        System.out.println(columId(150));
+    }
+
+
+    public static String columId(int a){
+        StringBuilder sb = new StringBuilder();
+        while (a > 0) {
+            int t = a % 26;
+            char c = (char) ('A' + t -1);
+            sb.append(c);
+            a /= 26;
+        }
+        return sb.reverse().toString();
+    }
 }
