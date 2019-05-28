d43a2d4784f8ba3792c3307d17d5da329e4cca70
chenzhe <zhe@eatigo.com>
2019-05-28 21:28:02 +0800

    solved num longest consecutive 1s

--- src/daily2019/D20190505.java
+++ src/daily2019/D20190505.java
@@ -8,7 +8,31 @@ For example, given 156, you should return 3.
 @Stripe
 
 @easy
+@solved
+@5min
+@bitop
+
+简单的bit 操作， & 1 == 1 意味着当前位位==1, 不停的右移即可 >>1
+
 
  */
 public class D20190505 {
+
+    public static void main(String[] args) {
+        System.out.println(longestBinary1s(156));
+    }
+
+    public static int longestBinary1s(int num) {
+        int res = 0;
+        int cur = 0;
+        while(num > 0) {
+            if ((num & 1) == 1) cur++;
+            else {
+                res = Math.max(cur, res);
+                cur = 0;
+            }
+            num >>= 1;
+        }
+        return res;
+    }
 }
