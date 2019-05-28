1487af50f23cdf8f0e42170b79d9617b46d98a2c
chenzhe <zhe@eatigo.com>
2019-05-28 21:33:39 +0800

    solved math

--- src/daily2019/D20190501.java
+++ src/daily2019/D20190501.java
@@ -11,7 +11,20 @@ Bonus: What input n <= 1000000 gives the longest sequence?
 
 @Apple
 @easy
+@solved
+@math
+因为每个even 不停的/2 最后肯定1
+每个odd，3n+1 == even 所以最后肯定1
+所以对的
+
+然后最长的，应该是 1000000 然后那个 odd 3n + 1 = 1000000,  999999/ 3 = 333333 最长
 
  */
 public class D20190501 {
+
+    public static void main(String[] args) {
+
+    }
+
+
 }
