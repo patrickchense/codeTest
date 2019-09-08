ef73f75d3ac61aa2dd8e046b215954af3e25cee4
chenzhe <zhe@eatigo.com>
2019-09-08 16:38:21 +0800

    amazon questions 2019

---
+++ src/amazon/ArrayWithXORandX.java
@@ -0,0 +1,26 @@
+package amazon;
+
+/*
+You are given two natural numbers n and x.
+You are required to create an array of n natural numbers such that the bitwise XOR of the numbers is equal to x.
+The sum of all the natural numbers that are available in the array is as minimum as possible.
+If their exists multiple arrays, then output the one that is the smallest among them.
+
+Example 1:
+
+Input: n = 3, x = 2
+Output: [1, 1, 2]
+Constraints:
+
+1<= n <=10^5
+1<= x <=10^18
+
+ */
+public class ArrayWithXORandX {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
--- src/amazon/TwoSumUniquePair.java
+++ src/amazon/TwoSumUniquePair.java
@@ -1,5 +1,9 @@
 package amazon;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 https://leetcode.com/discuss/interview-question/372434/Amazon-or-OA-2019-or-Two-Sum-Unique-Pairs
 Given an int array nums and an int target, find how many unique pairs in the array such that their sum is equal to target.
@@ -19,11 +23,45 @@ Output: 1
 Explanation:
 1 + 1 = 2
 
+@solved
  */
 public class TwoSumUniquePair {
     public static void main(String[] args) {
-
+        System.out.println(uniquePairsCount2(new int[]{1, 1, 2, 45, 46, 46}, 47));
+        System.out.println(uniquePairsCount2(new int[]{1, 1}, 2));
     }
 
+    // O(nlogn) O(1)
+    public static int uniquePairsCount(int[] nums, int target) {
+        int res = 0;
+        Arrays.sort(nums);
+        int i = 0, j = nums.length -1;
+        while (i < j) {
+            if (nums[i] + nums[j] == target) {
+                res++;
+                while(i + 1 < nums.length && nums[i] == nums[++i]);
+                while(j - 1 >= 0 && nums[j] == nums[--j]);
+            }
+            else if (nums[i] + nums[j] < target) i++;
+            else j--;
+        }
+        return res;
+    }
 
+    // O(n) O(n)
+    public static int uniquePairsCount2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int res = 0;
+        for (int i : nums) {
+            if (map.containsKey(target - i)) {
+                if (map.get(target - i) == 0) {
+                    res++;
+                    map.put(target - i, 1);
+                }
+            } else {
+                map.put(i, 0);
+            }
+        }
+        return res;
+    }
 }
