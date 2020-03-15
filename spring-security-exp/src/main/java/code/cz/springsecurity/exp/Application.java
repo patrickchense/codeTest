0da782a5e793857546e6fe9c032dcc279e29cd09
chenzhe <zhe.chen.sg@gmail.com>
2020-03-15 12:35:03 +0800

    spring boot exp & spring security

---
+++ spring-security-exp/src/main/java/code/cz/springsecurity/exp/Application.java
@@ -0,0 +1,21 @@
+package code.cz.springsecurity.exp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author zoday
+ * @version 1.0.0
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+
+    }
+
+}
---
+++ spring-security-exp/src/main/java/code/cz/springsecurity/exp/controller/HelloController.java
@@ -0,0 +1,30 @@
+package code.cz.springsecurity.exp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @version 1.0.0
+ *
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+}
